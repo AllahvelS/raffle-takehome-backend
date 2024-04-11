59c47ef2a0b99d00e8ea3458d554bf72ab322e7e
Allahvel Salisbury <allahvelsalisbury@pursuit.org>
2024-04-11 19:31:48 -0400

    Schema and seed files created successfully for database.

--- db/schema.sql
+++ db/schema.sql
@@ -3,31 +3,38 @@ CREATE DATABASE raffle_dev;
 
 \c raffle_dev
 
+DROP TABLE IF EXISTS winner;
+DROP TABLE IF EXISTS participants;
 DROP TABLE IF EXISTS raffle;
+
 CREATE TABLE raffle (
     id SERIAL PRIMARY KEY,
     name TEXT NOT NULL,
-    secret_token TEXT,
-    created_at TIMESTAMPTZ NOT NULL,
-    winner_id integer REFERENCES winner(id) ON DELETE CASCADE,
-)
+    secret_token TEXT NOT NULL,
+    created_at TIMESTAMPTZ NOT NULL
+);
 
-DROP TABLE IF EXISTS participants;
+
 CREATE TABLE participants (
     id SERIAL PRIMARY KEY,
     first_name TEXT NOT NULL,
     last_name TEXT NOT NULL,
     email VARCHAR(255) NOT NULL,
-    phone VARCHAR(15) NOT NULL UNIQUE,
+    phone VARCHAR(15),
     created_at TIMESTAMPTZ NOT NULL,
     raffle_id integer REFERENCES raffle(id) ON DELETE CASCADE,
-)
+    CONSTRAINT participants_required_feilds CHECK (
+        first_name IS NOT NULL AND
+        last_name IS NOT NULL AND 
+        email IS NOT NULL AND
+        raffle_id IS NOT NULL
+    )
+ );
 
-DROP TABLE IF EXISTS winner;
 CREATE TABLE winner (
     id SERIAL PRIMARY KEY,
     raffle_id integer REFERENCES raffle(id) ON DELETE CASCADE,
     participants_id integer REFERENCES participants(id) ON DELETE CASCADE
-)
+);
 
 CREATE INDEX participants_raffle_id ON participants(raffle_id);
